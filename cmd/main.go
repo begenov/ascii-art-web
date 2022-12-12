36747a82b24d7f4024abf57b12e00504692deb50
Your Name <you@example.com>
2022-12-12 20:26:29 +0600

    asf

--- cmd/main.go
+++ cmd/main.go
@@ -7,19 +7,19 @@ import (
 )
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	// /message := []byte("Hello, world!")
 	res, err := template.ParseFiles("templates/index.html", "templates/header.html")
-	// _, err := w.Write(message)
-	// fmt.Println("sadas")
-	// log.Fatal(err)
 	if err != nil {
 		fmt.Println(err)
 	}
 	res.ExecuteTemplate(w, "index", nil)
 }
 
+func ascii_art(w http.ResponseWriter, r *http.Request) {
+}
+
 func main() {
 	http.HandleFunc("/", viewHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/ascii-art", ascii_art)
 	http.ListenAndServe(":8080", nil)
 }
