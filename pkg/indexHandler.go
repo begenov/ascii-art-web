da1a6d5c0c3f63eca99c605262462561be48ddeb
Your Name <you@example.com>
2022-12-19 18:56:34 +0600

    asf

--- pkg/indexHandler.go
+++ pkg/indexHandler.go
@@ -11,18 +11,10 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if r.Method != http.MethodGet {
-		// Используем метод Header().Set() для добавления заголовка 'Allow: POST' в
-		// карту HTTP-заголовков. Первый параметр - название заголовка, а
-		// второй параметр - значение заголовка.
 		w.Header().Set("Allow", http.MethodPost)
-
-		// Вызываем метод w.WriteHeader() для возвращения статус-кода 405
-		// и вызывается метод w.Write() для возвращения тела-ответа с текстом "Метод запрещен".
 		w.WriteHeader(405)
 		w.Write([]byte("Метод запрещен!"))
 
-		// Затем мы завершаем работу функции вызвав "return", чтобы
-		// последующий код не выполнялся.
 		return
 	}
 	res, err := template.ParseFiles("templates/index.html", "templates/header.html") // parses HTML files
