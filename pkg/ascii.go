3dfa46d7eaefac0796df7c17a3541557f9f88467
Your Name <you@example.com>
2022-12-20 20:30:33 +0600

    asf

--- pkg/ascii.go
+++ pkg/ascii.go
@@ -1,6 +1,7 @@
 package art
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
 )
@@ -19,7 +20,7 @@ func Ascii(w http.ResponseWriter, r *http.Request) {
 		// Вызываем метод w.WriteHeader() для возвращения статус-кода 405
 		// и вызывается метод w.Write() для возвращения тела-ответа с текстом "Метод запрещен".
 		w.WriteHeader(405)
-		w.Write([]byte("Метод запрещен!"))
+		fmt.Fprint(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed))
 
 		// Затем мы завершаем работу функции вызвав "return", чтобы
 		// последующий код не выполнялся.
@@ -30,6 +31,21 @@ func Ascii(w http.ResponseWriter, r *http.Request) {
 	Check(err)
 	input := r.FormValue("input")        // create a variable for input
 	inputBanner := r.FormValue("banner") // create a variable for Banner
+	count := 2
+	for i := range r.Form {
+		if i != "banner" {
+			count--
+		} else if i != "input" {
+			count--
+		} else if count != 2 {
+			fmt.Println("ASd")
+			fmt.Fprint(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed))
+			return
+		} else {
+			continue
+		}
+	}
+
 	ASCII.Input = ""
 	arr, err := Startascii(input, inputBanner) // start ascii art
 	if err != nil {
