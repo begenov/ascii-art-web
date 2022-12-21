1132cf1f955383b18e9772786a0239909acc429e
Your Name <you@example.com>
2022-12-21 13:17:01 +0600

    asfd

--- pkg/handlers.go
+++ pkg/handlers.go
@@ -56,3 +56,20 @@ func Ascii(w http.ResponseWriter, r *http.Request) {
 	ASCII.Input += arr
 	res.ExecuteTemplate(w, "ascii", ASCII) //  run ascii.html
 }
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodPost)
+		w.WriteHeader(405)
+		w.Write([]byte("Метод запрещен!"))
+
+		return
+	}
+	res, err := template.ParseFiles("templates/index.html", "templates/header.html") // parses HTML files
+	Check(err)                                                                       // check for error
+	res.ExecuteTemplate(w, "index", nil)                                             // run index.html
+}
--- pkg/struct.go
+++ pkg/struct.go
@@ -1,6 +1,5 @@
 package art
 
-// struct ASCII
 var ASCII struct {
 	Input  string `json:"input"`
 	Banner string `json:"banner"`
